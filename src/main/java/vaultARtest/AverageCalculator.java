ab141d63bafea032d662bb50b575bcc808bfc5df
shivakvmar <shivareshi3353@gmail.com>
2024-12-12 16:35:28 +0530

    Revert changes from commit fc1dde0c7d623bdf2c0f816d3b05525b84be9212

--- src/main/java/vaultARtest/AverageCalculator.java
+++ src/main/java/vaultARtest/AverageCalculator.java
@@ -1,7 +1,5 @@
 package vaultARtest;
-
 public class AverageCalculator {
-
     public static void main(String[] args) {
         // Example array of numbers
         double[] numbers = {10, 20, 30, 40, 50};
@@ -17,17 +15,13 @@ public class AverageCalculator {
         // Output the result
         System.out.println("The average is: " + average);
     }
-}
 
 
-// New method added
 public void newMethod() {
-  System.out.println("New method added!");
    
 }
 
 
 // New method added
-public void shivaMethod() {
   System.out.println("New method added!");
 }
