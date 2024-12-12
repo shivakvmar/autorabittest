04d06897a20d7f3cc1fbcc46cb802d88dc6fbb65
shivakvmar.neela <shivakvmar.neela@autorabit.com>
2024-12-12 11:15:39 +0530

    added chrome

---
+++ src/main/java/vaultARtest/AverageCalculator.java
@@ -0,0 +1,20 @@
+package vaultARtest;
+
+public class AverageCalculator {
+
+    public static void main(String[] args) {
+        // Example array of numbers
+        double[] numbers = {10, 20, 30, 40, 50};
+
+        // Calculate the average
+        double sum = 0;
+        for (double num : numbers) {
+            sum += num;
+        }
+        
+        double average = sum / numbers.length;
+
+        // Output the result
+        System.out.println("The average is: " + average);
+    }
+}
