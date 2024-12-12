7d1154a0970a4819aada758cfefd26640c308524
shivakvmar <shivareshi3353@gmail.com>
2024-12-12 16:55:02 +0530

    Update AverageCalculator.java

--- src/main/java/vaultARtest/AverageCalculator.java
+++ src/main/java/vaultARtest/AverageCalculator.java
@@ -1,1 +1,18 @@
-cGFja2FnZSB2YXVsdEFSdGVzdDsKcHVibGljIGNsYXNzIEF2ZXJhZ2VDYWxjdWxhdG9yIHsKICAgIHB1YmxpYyBzdGF0aWMgdm9pZCBtYWluKFN0cmluZ1tdIGFyZ3MpIHsKICAgICAgICAvLyBFeGFtcGxlIGFycmF5IG9mIG51bWJlcnMKICAgICAgICBkb3VibGVbXSBudW1iZXJzID0gezEwLCAyMCwgMzAsIDQwLCA1MH07CgogICAgICAgIC8vIENhbGN1bGF0ZSB0aGUgYXZlcmFnZQogICAgICAgIGRvdWJsZSBzdW0gPSAwOwogICAgICAgIGZvciAoZG91YmxlIG51bSA6IG51bWJlcnMpIHsKICAgICAgICAgICAgc3VtICs9IG51bTsKICAgICAgICB9CiAgICAgICAgCiAgICAgICAgZG91YmxlIGF2ZXJhZ2UgPSBzdW0gLyBudW1iZXJzLmxlbmd0aDsKCiAgICAgICAgLy8gT3V0cHV0IHRoZSByZXN1bHQKICAgICAgICBTeXN0ZW0ub3V0LnByaW50bG4oIlRoZSBhdmVyYWdlIGlzOiAiICsgYXZlcmFnZSk7CiAgICB9Cgp9CgoKLy8gTmV3IG1ldGhvZCBhZGRlZApwdWJsaWMgdm9pZCBuZXdNZXRob2QoKSB7CiAgU3lzdGVtLm91dC5wcmludGxuKCJOZXcgbWV0aG9kIGFkZGVkISIpOwp9CgovLyBOZXcgbWV0aG9kIGFkZGVkCnB1YmxpYyB2b2lkIHNoaXZhTWV0aG9kKCkgewogIFN5c3RlbS5vdXQucHJpbnRsbigiTmV3IG1ldGhvZCBhZGRlZCEiKTsKfQ==
+package vaultARtest;
+public class AverageCalculator {
+    public static void main(String[] args) {
+        // Example array of numbers
+        double[] numbers = {10, 20, 30, 40, 50};
+
+        // Calculate the average
+        double sum = 0;
+        for (double num : numbers) {
+            sum += num;
+        }
+        
+        double average = sum / numbers.length;
+
+        // Output the result
+        System.out.println("The average is: " + average);
+    }
+}
