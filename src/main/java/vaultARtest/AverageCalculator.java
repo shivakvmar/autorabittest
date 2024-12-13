8867590659ea534781014018e38909b057d69531
shivakvmar <shivareshi3353@gmail.com>
2024-12-13 08:08:45 +0530

    Update AverageCalculator.java

--- src/main/java/vaultARtest/AverageCalculator.java
+++ src/main/java/vaultARtest/AverageCalculator.java
@@ -5,14 +5,11 @@ public class AverageCalculator {
         // Calculate the average
 double average = sum / numbers.length;
 double average = sum / numbers.length;
-double average = sum / numbers.length;
+
 
         double sum = 0;
         for (double num : numbers) {
             sum += num;
         System.out.println("The average is: " + average);
         }}}
-// New method added
-public void shivaramkumarMethod() {
-  System.out.println("New method added!");
-}
+
