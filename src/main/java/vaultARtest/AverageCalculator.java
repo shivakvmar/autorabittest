429e6059f84fd1da10ec01fd2bd28fa5ee654186
shivakvmar <shivareshi3353@gmail.com>
2025-01-02 20:20:07 +0530

    Added new method to AverageCalculator.java

--- src/main/java/vaultARtest/AverageCalculator.java
+++ src/main/java/vaultARtest/AverageCalculator.java
@@ -2,6 +2,7 @@ package vaultARtest;
 public class AverageCalculator {
     public static void main(String[] args) {
         double[] numbers = {10, 20, 30, 40, 50};
+// This is a comment at line 5
 
         // Calculate the average
 double average = sum / numbers.length;
@@ -13,4 +14,7 @@ double average = sum / numbers.length;
         for (double num : numbers) {
             sum += num;
         System.out.println("The average is: " + average);
-        }}}
+        }}// New method added
+public void shivaramkumarMethod() {
+  System.out.println("New method added!");
+}}
