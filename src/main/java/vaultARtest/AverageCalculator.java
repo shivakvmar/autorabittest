7223697239f2fdb551da2eeac1806d193bf4f690
shivakvmar.neela <shivakvmar.neela@autorabit.com>
2025-01-02 18:31:57 +0530

    Revert "Added new method to AverageCalculator.java"
    This reverts commit e8dbc79b4d7dcb52c1df6a5d6207ab4747e5b56a.

--- src/main/java/vaultARtest/AverageCalculator.java
+++ src/main/java/vaultARtest/AverageCalculator.java
@@ -3,7 +3,6 @@ public class AverageCalculator {
     public static void main(String[] args) {
         double[] numbers = {10, 20, 30, 40, 50};
 // This is a comment at line 5
-// This is a comment at line 5
         // Calculate the average
 double average = sum / numbers.length;
 double average = sum / numbers.length;
@@ -14,7 +13,4 @@ double average = sum / numbers.length;
         for (double num : numbers) {
             sum += num;
         System.out.println("The average is: " + average);
-        }// New method added
-public void shivaramkumarMethod() {
-  System.out.println("New method added!");
-}}
+        }}
